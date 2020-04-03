bd66c0c3996fa2b6cc30f41a5b348ca948e3fb31
Yani Rodriguez <yani010712@gmail.com>
2020-04-03 17:01:19 -0400

    gitignore file

--- target/cucumber-html-report/report.js
+++ target/cucumber-html-report/report.js
@@ -50,4 +50,142 @@ formatter.result({
 formatter.after({
   "status": "passed"
 });
+formatter.scenario({
+  "name": "Login functionality exists",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@smokeUAT"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I have open the browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Login_steps.iHaveOpenTheBrowser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I open Facebook website",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Login_steps.iOpenFacebookWebsite()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Login button should exit",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Login_steps.loginButtonShouldExit()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Login functionality exists",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@smokePREPROD"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I have open the browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Login_steps.iHaveOpenTheBrowser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I open Facebook website",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Login_steps.iOpenFacebookWebsite()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Login button should exit",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Login_steps.loginButtonShouldExit()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Login functionality exists",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@smokePROD"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I have open the browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Login_steps.iHaveOpenTheBrowser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I open Facebook website",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Login_steps.iOpenFacebookWebsite()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Login button should exit",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Login_steps.loginButtonShouldExit()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
 });
